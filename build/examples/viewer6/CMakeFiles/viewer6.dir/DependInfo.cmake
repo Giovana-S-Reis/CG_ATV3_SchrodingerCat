d612da6a3bdbb020d8d8b45e0dc8f55f6fcf6f76
bruno.rabello <bruno.rabello@gitlab.qitech.com.br>
2023-12-05 01:06:53 -0300

    First push

---
+++ build/examples/viewer6/CMakeFiles/viewer6.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/brunobrabello/Projetos/CG-ATV3/examples/viewer6/main.cpp" "examples/viewer6/CMakeFiles/viewer6.dir/main.cpp.o" "gcc" "examples/viewer6/CMakeFiles/viewer6.dir/main.cpp.o.d"
+  "/home/brunobrabello/Projetos/CG-ATV3/examples/viewer6/model.cpp" "examples/viewer6/CMakeFiles/viewer6.dir/model.cpp.o" "gcc" "examples/viewer6/CMakeFiles/viewer6.dir/model.cpp.o.d"
+  "/home/brunobrabello/Projetos/CG-ATV3/examples/viewer6/trackball.cpp" "examples/viewer6/CMakeFiles/viewer6.dir/trackball.cpp.o" "gcc" "examples/viewer6/CMakeFiles/viewer6.dir/trackball.cpp.o.d"
+  "/home/brunobrabello/Projetos/CG-ATV3/examples/viewer6/window.cpp" "examples/viewer6/CMakeFiles/viewer6.dir/window.cpp.o" "gcc" "examples/viewer6/CMakeFiles/viewer6.dir/window.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/brunobrabello/Projetos/CG-ATV3/build/abcg/CMakeFiles/abcg.dir/DependInfo.cmake"
+  "/home/brunobrabello/Projetos/CG-ATV3/build/abcg/external/fmt/CMakeFiles/fmt.dir/DependInfo.cmake"
+  "/home/brunobrabello/Projetos/CG-ATV3/build/abcg/external/imgui/CMakeFiles/imgui.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/examples/viewer6/CMakeFiles/viewer6.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../../bin/viewer6"
+  "../../bin/viewer6.pdb"
+  "CMakeFiles/viewer6.dir/main.cpp.o"
+  "CMakeFiles/viewer6.dir/main.cpp.o.d"
+  "CMakeFiles/viewer6.dir/model.cpp.o"
+  "CMakeFiles/viewer6.dir/model.cpp.o.d"
+  "CMakeFiles/viewer6.dir/trackball.cpp.o"
+  "CMakeFiles/viewer6.dir/trackball.cpp.o.d"
+  "CMakeFiles/viewer6.dir/window.cpp.o"
+  "CMakeFiles/viewer6.dir/window.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/viewer6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
